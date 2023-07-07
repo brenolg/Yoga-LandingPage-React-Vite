14654e8014abbc2c137f1ec7cb3bc5925e27f007
CaioGod <caioafgod@gmail.com>
2023-07-07 17:24:58 -0300

    fix: removing blur

--- src/components/SealSection.jsx
+++ src/components/SealSection.jsx
@@ -6,7 +6,6 @@ import seal from "../images/seal.svg";
 export function SealSection() {
   return (
     <SealSectionContainer isMobile={isMobile}>
-      <BackgroundBlur isMobile={isMobile} />
       <img src={seal} />
       <p className={isMobile ? "text-sm" : "text-md"}>
         Nos do Yoga iClub criamos o 15payback, que da a você 15 dias para solicitar seu reembolso sem questionamento ou burocracia alguma caso não esteja satisfeito com o produto.
@@ -15,39 +14,7 @@ export function SealSection() {
   )
 }
 
-const BackgroundBlur = styled.div`
-  background: var(--C2);
-  border-radius: 1180px;
-  filter: blur(27.5px);
-  z-index: -1;
-  -webkit-filter: blur(27.5px);
-  position: absolute;
-  height: 100%;
-  &:before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    backdrop-filter: blur(5px);
-  }
-
-  ${props => props.isMobile ? 
-  `
-    width: 100%;
-    height: 100%;
-  `
-    :
-  `
-    width: 130%;
-    height: 100%;
-    margin: -100px;
-  `
-  }
-`
-
 const SealSectionContainer = styled.div`
-  filter: blur(0px);
-  -webkit-filter: blur(0px);
   color: var(--C4);
   border-radius: 1180px;
   flex-direction: column;
