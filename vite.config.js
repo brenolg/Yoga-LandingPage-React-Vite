5d4f08ce4d62a1a36f31e1d8e3d9ac1dc141a1d0
CaioGod <caioafgod@gmail.com>
2023-07-25 18:14:06 -0300

    fix: deploy credentials

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { viteS3 } from 'vite-plugin-s3';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Yoga-LandingPage-React-Vite',
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  if (command === 'build' && env.AWS_ACCESS_KEY_ID && env.AWS_SECRET_ACCESS_KEY) {
+    return {
+      plugins: [
+        react(),
+        viteS3({
+          s3Options: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            region: 'us-east-1',
+          },
+          s3UploadOptions: {
+            Bucket: 'yoga-landing-page',
+          },
+        }),
+      ],
+    };
+  } else {
+    return {
+      base: '/Yoga-LandingPage-React-Vite',
+      plugins: [
+        react(),
+      ],
+    };
+  }
 });
