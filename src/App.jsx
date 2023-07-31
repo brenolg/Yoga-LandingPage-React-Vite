bac7ebc4bcf5ed6d3545aee7945ecb13b5815bc7
brenolg <brenolg@yahoo.com.br>
2023-07-31 17:55:09 -0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Prices from './components/Prices/Prices';
 import QuestionsCarrousel from './components/QuestionsCarrousel/QuestionsCarrousel';
 import ReviewsCarrousel from './components/ReviewsCarrousel/ReviewsCarrousel';
 import Seal from './components/Seal/Seal';
-// Responsive check:  Header/ hero / Questions / pillars / access / moment /reviews / doubts / seal /
+
 function App() {
   const isTabletAndMobile = useMediaQuery({ query: '(max-width: 1127px)' });
   const isMobile = useMediaQuery({ query: '(max-width: 744px)' });
