37a7bc9b9718d44660b2fc03a72ba4cbe720ae6e
CaioGod <caioafgod@gmail.com>
2023-11-09 14:26:27 -0300

    feat: updating links

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -25,7 +25,7 @@ export default function Prices({ isTabletAndMobile, isMobile }) {
                   title="iClub Pro"
                   subTitle="(anual)"
                   price="R$ 180,00/mês"
-                  link="https://pay.hotmart.com/J71174272X?off=pbc0omd1"
+                  link="https://pay.hotmart.com/J71174272X?off=0x1wq6ay"
                   list={[
                     'Acesso as aulas AO VIVO 2x por semana',
                     'Acesso a plataforma com aulas gravadas',
@@ -45,7 +45,7 @@ export default function Prices({ isTabletAndMobile, isMobile }) {
                   title="iClub"
                   subTitle="anual"
                   price="R$ 39,90/mês"
-                  link="https://pay.hotmart.com/J71174272X?off=wl675tvr"
+                  link="https://pay.hotmart.com/J71174272X?off=y7ewtrqw"
                   list={[
                     'Acesso a plataforma com aulas gravadas',
                     'Temas novos todos os meses',
@@ -86,7 +86,7 @@ export default function Prices({ isTabletAndMobile, isMobile }) {
                   title="iClub"
                   subTitle="anual"
                   price="R$ 39,90/mês"
-                  link="https://pay.hotmart.com/J71174272X?off=wl675tvr"
+                  link="https://pay.hotmart.com/J71174272X?off=y7ewtrqw"
                   list={[
                     'Acesso a plataforma com aulas gravadas',
                     'Temas novos todos os meses',
@@ -106,7 +106,7 @@ export default function Prices({ isTabletAndMobile, isMobile }) {
                   title="iClub Pro"
                   subTitle="(anual)"
                   price="R$ 180,00/mês"
-                  link="https://pay.hotmart.com/J71174272X?off=pbc0omd1"
+                  link="https://pay.hotmart.com/J71174272X?off=0x1wq6ay"
                   list={[
                     'Acesso as aulas AO VIVO 2x por semana',
                     'Acesso a plataforma com aulas gravadas',
